89d34e061985dae5b4da0979d3c1e44f7894f33a
Claire Bartholomew <claire.bartholomew@metoffice.gov.uk>
2020-01-06 11:48:23 +0000

    options for job 2126061

--- train_svg.sh
+++ train_svg.sh
@@ -8,4 +8,4 @@ source /nobackup/sccsb/miniconda3/bin/activate /nobackup/sccsb/miniconda3/envs/s
 
 # Python script with all parameters
 #python ./svg/generate_svg_lp.py --model_path ./svg/pretrained_models/svglp_bair.pth --log_dir ./svg/images --data_root /nobackup/sccsb/bair > log_svg.out
-python train_svg_lp_test.py --dataset radar --beta 0.0001 --epoch_size 2772 --n_past 5 --n_future 25 --data_root /nobackup/scssb/radar 
+python train_svg_lp_test.py --dataset radar --beta 0.0001 --epoch_size 2772 --n_past 3 --n_future 7 --data_root /nobackup/scssb/radar 
