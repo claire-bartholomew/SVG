6d10f6718ba19d44fd735c2854bd571a60088580
Claire Bartholomew <claire.bartholomew@metoffice.gov.uk>
2022-01-04 16:41:31 +0000

    last changes pre-christmas

--- train_svg_dec.sh
+++ train_svg_dec.sh
@@ -9,4 +9,4 @@ unset GOMP_CPU_AFFINITY KMP_AFFINITY
 # Python script with all parameters
 #python train_svg_lp_cuda.py --dataset bair --model vgg --g_dim 128 --z_dim 64 --beta 0.0001 --n_past 2 --n_future 10 --channels 3 --data_root /nobackup/sccsb/bair/ --log_dir logs
 #python ./svg/generate_svg_lp.py --model_path ./svg/pretrained_models/svglp_bair.pth --log_dir ./svg/images --data_root /nobackup/sccsb/bair > log_svg.out
-python train_svg_lp_dec_augment.py --dataset radar --beta 0.0001 --epoch_size 2772 --n_past 3 --n_future 7 --data_root /nobackup/scssb/radar
+python train_svg_lp_dec_augment.py --dataset radar --beta 0.0001 --epoch_size 128 --n_past 3 --n_future 7 --data_root /nobackup/scssb/radar
